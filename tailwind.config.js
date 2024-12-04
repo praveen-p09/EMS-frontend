a4a3ff0b7ae1237aae3373ab2f23a4e3ce67de59
Praveen Patro <praveen.patro911@gmail.com>
2024-12-04 15:06:08 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/shadcn-ui/dist/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
